22512904e8d647e535f322784c26cabea6f5c4a2
Vailson dos Santos Silva <vailsoft@hotmail.com>
2021-04-20 09:00:55 -0300

    Adicionado mais palavras e uns comentários

--- forca.js
+++ forca.js
@@ -1,53 +1,56 @@
 var dica, words,random,palavraCerta,btnDica,posicao,letra,posicao,letrasUsadas,divLetrasUsadas,tamanhoPalavraCerta,resultado;
 
+// Gerador de número aleatório
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+// Recarrega a página
 function reload(){
     window.location.reload();
 }
 
 function inicia(){
-    letra = document.getElementById("letra"); 
     posicao = document.getElementsByClassName("posicao");
-    divLetrasUsadas = document.getElementById("letrasUsadas"); 
-    letrasUsadas = [];
     divPalavra = document.getElementById("divPalavra");
-    words = [
-        ["apocalipse","Livro da Bíblia"], 
-        ["submarino","Veículo maritmo"], 
-        ["ornitorrinco","Mamífero"], 
-        ["dinossauro","Réptil"], 
-        ["saturno","Planeta"], 
-        ["megabite","Unidade de Medida"], 
-        ["madagascar", "Filme"], 
-        ["pernambuco","Estado"], 
-        ["banana","Fruta"]];
+    divLetrasUsadas = document.getElementById("letrasUsadas"); 
+    letra = document.getElementById("letra"); 
+
+    letrasUsadas = [];
+    words = [["apocalipse","Livro da Bíblia"],["genessis","Livro da Bíblia"],["mateus","Livro da Bíblia"], ["tessalonicenses","Livro da Bíblia"], ["submarino","Veículo maritmo"], ["ornitorrinco","Mamífero"], ["dinossauro","Réptil"], ["saturno","Planeta"],["urano","Planeta"], ["megabite","Unidade de Medida"], ["madagascar", "Filme"], ["pernambuco","Estado"], ["banana","Fruta"],["internacional", "Time de Futebol"],["engenheiro","Profissão"],["programador","Profissão"],["uruguai","País"],["portugal","País"],["espanha","País"],["japones","Idioma"]];
     random = getRandomInt(0,words.length);
     palavraCerta = words[random][0];
     tamanhoPalavraCerta = palavraCerta.length;
+    dica = words[random][1];
+    
     for(var i=0; i < tamanhoPalavraCerta; i++){
         divPalavra.innerHTML += "<input type='text' class='posicao' readonly>";
     }
-    dica = words[random][1];
+
     document.getElementById("btnDica").addEventListener("click",function(){alert(dica)});
     document.getElementById("btnVerifica").addEventListener("click", verifica);  
       
 }
 
 function verifica(){
+    // Verifica se a letra digitada está contida em alguma posição da palavra certa
     for(var i = 0; i < tamanhoPalavraCerta; i++){
         if(letra.value == palavraCerta[i]){
             posicao[i].value = letra.value;
         }
     }
+
+    // Verifica destina as letras que não estão contidas na palavra correta para um array e impede de adicionar letras repetidas no mesmo array
     if(palavraCerta.indexOf(letra.value) < 0 && letrasUsadas.indexOf(letra.value) < 0){
         letrasUsadas.push(letra.value);
     }
     divLetrasUsadas.innerHTML = "Letras Usadas: "+letrasUsadas.join();
+    
     letra.value = "";
+    
+    // Torna visível as partes do boneco
     switch(letrasUsadas.length){
         case 1:
             document.getElementById("cabeca").style.display = "block";
@@ -70,13 +73,14 @@ function verifica(){
             break;
         case 7:
             document.getElementById("died").style.display = "block";
+            // Mostra mensagem quando perde
             alert("Faleceu!!! a palavra era \""+palavraCerta+"\"");
             // window.location.reload();
             break;
     }
     
+    // Mostra mensagem quando você ganha
     resultado = 0;
-
     for(i=0; i<tamanhoPalavraCerta; i++){
         if(posicao[i].value.length > 0){
             resultado += 1;
